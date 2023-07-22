b1b50f872b09350d5b9dbe5c5ec1430f365f3cd2
marlonsalazarg <marlon456_456@hotmail.com>
2023-07-22 18:09:31 -0500

    add: omniauth with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: :callbacks }
   post "/new_user", to: "users#create"
   resources :users
   resources :genres
