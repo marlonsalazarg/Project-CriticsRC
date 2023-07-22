288ec6b9c11394b908474801e082239d5dfca95b
marlonsalazarg <marlon456_456@hotmail.com>
2023-07-22 17:23:13 -0500

    add: views forms devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :username, presence: true, uniqueness: true, length: { maximum: 16 }
   class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -11,7 +12,6 @@ class User < ApplicationRecord
     # Validations
     # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   
-    validates :username, presence: true, uniqueness: true, length: { maximum: 16 }
     validates :email, presence: true, uniqueness: true
     # validates :email, format: { with: VALID_EMAIL_REGEX }, allow_blank: true
   
